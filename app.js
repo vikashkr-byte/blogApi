03d2581575b5d0f7dac6256dc686cbf7006153cc
vikashkr-byte <vikashkr4git@gmail.com>
2022-12-28 15:54:57 +0530

    bloggihg api

--- app.js
+++ app.js
@@ -7,5 +7,8 @@ const app = express()
 app.use(express.json())
 app.use("/api/user", router)
 app.use("/api/blog", blogRouter)
+app.get("/",(req,res)=>{
+    res.send("welcome")
+})
 mongoose.set('strictQuery', false);
 mongoose.connect("mongodb+srv://admin:k@cluster0.at8tjjr.mongodb.net/Blog?retryWrites=true&w=majority").then(() => app.listen(5000)).then(() => console.log("listening on http://localhost:5000")).catch((err) => console.log('err:', err))
